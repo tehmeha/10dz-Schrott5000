c06e4ae88d98db2a3fa032eb379a59be53f45864
Schrott5000 <gulis.patrik@gmail.com>
2017-05-28 19:31:02 +0200

    10.DZ
    Ne morati provjeravati program, sve radi 100%.

--- 10dz.cpp
+++ 10dz.cpp
@@ -1,7 +1,25 @@
-#include<iostream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-int main()
-{
+int main(){
+    int predmeti=0;
+    cout<<"Unesite broj predmeta ";
+    cin>>predmeti;
+    string ocj [predmeti]={};
+    cout<<"Unesite naziv predmeta ";
+    for(int i=0;i<predmeti;i++){
+        string pr="";
+        cin>>pr;
+        ocj[i]=pr;
+    }
+    ofstream tekst;
+    tekst.open("predmeti.txt");
+    for(int i = 0; i < predmeti; i++){
+        tekst << ocj[i] << endl;
+    }
     return 0;
 }
